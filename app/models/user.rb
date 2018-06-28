231e3b2df988f40dff42d202411a65dcfdaa0d7a
eduardogz94 <ezangarini@hotmail.com>
2018-06-28 16:34:51 -0400

    Structing app

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    before_save { username.downcase }
+    
+    validates :username,  presence: true,
+    uniqueness: { case_sensitive: false }
+    
+    validates :password, presence: true, length: { minimum: 6 }
+    
+    has_secure_password
+
+end
