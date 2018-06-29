625131df54c2ee4f2bdb729fba4ccace42dce5b7
eduardogz94 <ezangarini@hotmail.com>
2018-06-28 21:58:03 -0400

    Testing rails API

--- dist/app/controllers/api/v1/users_controller.rb
+++ dist/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
+  
   class UsersController < ApplicationController
+    
     def index
       @users = User.all
       render json: @users
@@ -10,24 +12,37 @@ module Api::V1
       render json: @user
     end
 
-    def new
-        
-    end
-
+    
     def create
       @user = User.new(user_params)
+        if @user.save
+          render json: @user
+        else
+          render 'new'
+        end
+    end
+      
+    def update
+      @user = User.update(user_params)
       if @user.save
         render json: @user
-        # Handle a successful save.
       else
-        render 'new'
       end
 
+    end
+
+    def destroy  
+      @user = User.delete(params[:id])
+        if @user.save
+          render json: @user
+        else
+          render 'false'
+        end  
     end
 
     def user_params
       params.require(:user).permit(:username,:password,:password_confirmation)
     end
 
-    end
+  end
 end
--- dist/config/routes.rb
+++ dist/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   
   namespace :api do
-    namespace :v1 do
-      resources :users  
+    namespace :v1 do 
+      root 'users#index'
+      get 'users/show'
+      post 'users/create'
+      patch 'users/update'
+      delete 'users/destroy'
     end
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
