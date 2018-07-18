32612e0ea6123e397fda2dbaf76b4151b5d15ea0
cbrzn <cesarbrazon10@gmail.com>
2018-07-18 01:54:14 -0400

    fixing bug

--- react-native/components/Extra/MyIp.js
+++ react-native/components/Extra/MyIp.js
@@ -1,1 +1,2 @@
-export const myIp = `10.172.175.104:4000`
+// export const myIp = `10.172.175.104:4000`
+export const myIp = `172.20.10.4:4000`
--- react-native/components/Users/SignupForm.js
+++ react-native/components/Users/SignupForm.js
@@ -68,8 +68,9 @@ export default class SignupForm extends Component {
         
 			
             if (!result.cancelled) {
-				const type = result.uri.substr(result.uri.indexOf('.')+1)
-				console.log(type)
+				let type = result.uri.substr(result.uri.indexOf('.')+1)
+				type = (type == 'jpg') ? 'jpeg' : type 
+				alert(type)
 				this.setState({ image:result.base64, show:result.uri, type });
             }
         // }).catch(err => {
